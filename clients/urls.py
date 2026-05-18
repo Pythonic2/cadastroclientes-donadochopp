0e15e445bae86eeb6ce160354bcf38d0e04c8f60
igormarinhos <igor.marinho@dd8.ai>
2026-05-18 13:22:40 -0300

    client para clients url

--- clients/urls.py
+++ clients/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r"client", views.UserViewSet)
+router.register(r"clients", views.UserViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
