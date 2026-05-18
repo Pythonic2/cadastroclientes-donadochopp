3813db5b8d8c1a85894ad399e1f3a292da2e9338
igormarinhos <igor.marinho@dd8.ai>
2026-05-18 17:24:48 -0300

    add 2 fields in models client

--- clients/models.py
+++ clients/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
 class Client(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    phone_number = models.CharField(max_length=20)
     data_criada = models.DateTimeField(auto_now_add=True)
-    
+    TRABALHA_CHOICES = [
+        ("Sim", "Sim"),
+        ("Não", "Não"),
+    ]
+    trabalha = models.CharField(max_length=3, choices=TRABALHA_CHOICES, default="Não")
+    nome_empresa = models.CharField(max_length=255, blank=True, null=True)
+
     def __str__(self):
         return self.name
--- clients/serializers.py
+++ clients/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Client
-        fields = ["url", "name", "email", "phone_number"]
+        fields = ["url", "name", "email", "phone_number", "trabalha", "nome_empresa"]
